93615fcc61a4c7a3a5e079415fb1a2e18a5b1d3d
wjakelee <j.wlee@hotmail.com>
2020-08-19 08:35:51 -0700

    fixed error when creating tables

--- migration.js
+++ migration.js
@@ -28,7 +28,7 @@ db.serialize(function() {
   db.run(`DROP TABLE IF EXISTS Menu`);
   db.run(`CREATE TABLE IF NOT EXISTS Menu(
     id INTEGER PRIMARY KEY NOT NULL,
-    title TEXT NOT NULL,
+    title TEXT NOT NULL
     )`);
 
 
