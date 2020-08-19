537650c00f5f197e3db562813d8f0ca718e78afe
wjakelee <j.wlee@hotmail.com>
2020-08-19 07:56:23 -0700

    created server and api directory

---
+++ api/api.js
@@ -0,0 +1,7 @@
+const express = require('express');       //import express
+const apiRouter = express.Router();       //create api router
+
+
+
+
+module.exports = apiRouter;               //export api router
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');               //import express
+const bodyParser = require('body-parser');        //body parser module
+const errorhandler = require('errorhandler');     //errorhandle module
+const cors = require('cors');                     //cross orgin resource sharing
+const morgan = require('morgan');                 //morgan logger module
+
+const apiRouter = require('./api/api');           //import api router from api directory
+
+const app = exprees();
+const PORT = process.env.PORT || 4000;    //create port 4000 (where front-end will make requests)
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use('/api', apiRouter);             //all routes that have endpoint /api will be sent to apiRouter
+
+app.use(errorHandler());
+
+app.listen(PORT, () => {
+  console.log(`Listing on PORT ${PORT}`);   //listen on PORT 4000
+})
+
+module.exports = app;         //export express app
