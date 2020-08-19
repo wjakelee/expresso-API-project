759aaade5632586b0bb7fc1ea3bf9e5b6725b80a
wjakelee <j.wlee@hotmail.com>
2020-08-19 16:05:31 -0700

    created POST route to add menus

--- api/menus.js
+++ api/menus.js
@@ -5,8 +5,44 @@ const sqlite3 = require('sqlite3');             //import database
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 
+//GET route retrieves all current menus
+menusRouter.get('/', (req, res, next) => {
+  db.all(`SELECT * FROM Menu`, (error, menus) => {
+    if (error){
+      next(error);
+    }
+    res.status(200).json({ menus: menus });
+  })
+})
 
 
+//POST route adds a menu to the Menu table if all require parameters exist
+menusRouter.post('/', (req, res, next) => {
+  const title = req.body.menu.title;
+  if (!title){
+    res.sendStatus(400);                     //request has incorrect parameters
+  }
+
+  //insert new menu into Menu table
+  db.run(`INSERT INTO Menu (title) VALUES ($title)`,
+    {
+      $title: title,
+    },
+    function(error){
+      if (error){
+        next(error);
+      } else {
+        db.get(`SELECT * FROM Menu WHERE id = ${this.lastID}`,      //retrieve last added menu
+          (error, menu) => {
+            if (error){
+              next(error);
+            }
+            res.status(201).json({ menu: menu });     //send last added menu
+          })
+      }
+    }
+  );
+});
 
 
 module.exports = menusRouter;
