935e1b41c4f1235d1d00d1299316ca3a403a2ca8
wjakelee <j.wlee@hotmail.com>
2020-08-19 07:31:42 -0700

    Create tables in database

---
+++ migration.js
@@ -0,0 +1,48 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./database.sqlite');
+
+db.serialize(function() {
+  //create Employee table
+  db.run(`DROP TABLE IF EXISTS Employee`);
+  db.run(`CREATE TABLE IF NOT EXISTS Employee(
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    position TEXT NOT NULL,
+    wage INTEGER NOT NULL,
+    is_current_employee INTEGER DEFAULT 1
+    )`);
+
+  //create Timesheet table
+  db.run(`DROP TABLE IF EXISTS Timesheet`);
+  db.run(`CREATE TABLE IF NOT EXISTS Timesheet(
+    id INTEGER PRIMARY KEY NOT NULL,
+    hours INTEGER NOT NULL,
+    rate INTEGER NOT NULL,
+    date INTEGER NOT NULL,
+    employee_id INTEGER NOT NULL,
+    FOREIGN KEY(employee_id) REFERENCES Employee(id)
+    )`);
+
+
+  //create Menu table
+  db.run(`DROP TABLE IF EXISTS Menu`);
+  db.run(`CREATE TABLE IF NOT EXISTS Menu(
+    id INTEGER PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    )`);
+
+
+  //create Menu Item table
+  db.run(`DROP TABLE IF EXISTS MenuItem`);
+  db.run(`CREATE TABLE IF NOT EXISTS MenuItem(
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT,
+    inventory INTEGER NOT NULL,
+    price INTEGER NOT NULL,
+    menu_id INTEGER NOT NULL,
+    FOREIGN KEY(menu_id) REFERENCES Menu(id)
+    )`);
+
+
+});
