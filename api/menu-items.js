f2fd84215688bdae0635b01c77acb6273313b9cb
wjakelee <j.wlee@hotmail.com>
2020-08-21 09:02:55 -0700

    added GET route to retrieve all menu items for specific menu

--- api/menu-items.js
+++ api/menu-items.js
@@ -1,11 +1,22 @@
-const express = require('express');                 //import express
-const menuItemsRouter = express.Router();           //create menu items router
+const express = require('express');                               //import express
+const menuItemsRouter = express.Router({mergeParams: true});      //create menu items router
 
 const sqlite3 = require('sqlite3');             //import database
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 
-
+//GET route retrieves all current menu items
+menuItemsRouter.get('/', (req, res, next) => {
+  db.all(`SELECT * FROM MenuItem WHERE menu_id = $menuId`,
+    { $menuId: req.params.menuId },
+    (error, items) => {
+      if (error){
+        next(error);
+      }
+      res.status(200).json({ menuItems: items });
+    }
+  )
+})
 
 
 module.exports = menuItemsRouter;       //export menu items router
